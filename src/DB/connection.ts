ad0070062497ad1940f6a566d39b0c2e2c2ec0e0
Itthithee <itth@Itthithees-MacBook-Pro.local>
2020-02-05 21:53:53 +0700

    change database configuration

--- src/DB/connection.ts
+++ src/DB/connection.ts
@@ -1,25 +1,18 @@
 import { Pool } from 'pg';
-
+import { config } from './dbconfig'
 class Connect {
-    private pool: Pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'Hospital',
-        password: '1234',
-        port: 5432,
-    }
-    );
+    private pool: Pool = new Pool(config);
     public row: any[];
     async query(text: string) {
         try {
             let res = await this.pool.query(text)
             console.log(res);
-            
+
             return res.rows
         }
         catch (err) {
             console.log(err);
-            
+
             return err
         }
 
